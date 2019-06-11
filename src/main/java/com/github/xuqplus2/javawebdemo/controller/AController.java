64692ae5d39153ea25b916a53b4f88829a7670e1
xuqplus <445172495@qq.com>
2019-06-11 17:50:20 +0800

    swaggerui

--- src/main/java/com/github/xuqplus2/javawebdemo/controller/AController.java
+++ src/main/java/com/github/xuqplus2/javawebdemo/controller/AController.java
@@ -3,6 +3,7 @@ package com.github.xuqplus2.javawebdemo.controller;
 import com.github.xuqplus2.javawebdemo.domain.User;
 import com.github.xuqplus2.javawebdemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,12 @@ public class AController {
 
   @Autowired UserRepository userRepository;
 
-  @GetMapping
+  @Value("${git.commit.version}")
+  String version;
+
+  @GetMapping("version")
   public String a() {
-    return "ok";
+    return version;
   }
 
   @GetMapping("user")
