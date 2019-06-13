8353abe3a4562112813536a02df745524d7ce9a7
xuqplus <445172495@qq.com>
2019-06-13 13:31:05 +0800

    uploadFile

--- src/main/java/com/github/xuqplus2/javawebdemo/controller/test/TestFetchController.java
+++ src/main/java/com/github/xuqplus2/javawebdemo/controller/test/TestFetchController.java
@@ -3,12 +3,23 @@ package com.github.xuqplus2.javawebdemo.controller.test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 @RestController
 @RequestMapping("test/fetch")
 public class TestFetchController {
+
+  private static final String uploadFileDir = String.format("uploadFileDir%s", File.separator);
+
+  static {
+    if (!new File(uploadFileDir).exists()) {
+      new File(uploadFileDir).mkdirs();
+    }
+  }
 
   private static final Logger LOGGER = LoggerFactory.getLogger(TestFetchController.class);
 
@@ -21,5 +32,12 @@ public class TestFetchController {
   public Object withParam(@RequestBody Map body) {
     LOGGER.info("body={}", body);
     return body;
+  }
+
+  @PostMapping("uploadFile")
+  public Object uploadFile(MultipartFile file) throws IOException {
+    LOGGER.info("file.getOriginalFilename()={}", file.getOriginalFilename());
+    file.transferTo(new File(uploadFileDir + file.getOriginalFilename()).toPath());
+    return String.format("{\"filename\":\"%s\",\"status\":\"ok\"}", file.getOriginalFilename());
   }
 }
