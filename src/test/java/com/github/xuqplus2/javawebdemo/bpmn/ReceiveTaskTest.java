c538244b2a506d3b62c0e9a14415d5d7eb6bd026
xuqplus <445172495@qq.com>
2019-07-11 18:52:09 +0800

    receiveTask

---
+++ src/test/java/com/github/xuqplus2/javawebdemo/bpmn/ReceiveTaskTest.java
@@ -0,0 +1,59 @@
+package com.github.xuqplus2.javawebdemo.bpmn;
+
+import com.github.xuqplus2.javawebdemo.JavaWebDemoApplicationTests;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.runtime.Execution;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ReceiveTaskTest extends JavaWebDemoApplicationTests {
+
+  @Autowired
+  ProcessEngine processEngine;
+
+  @Test
+  public void deploymentProcessDefinition() {
+    Deployment deployment = processEngine.getRepositoryService()
+            .createDeployment()
+            .name("接收任务测试")
+            .addClasspathResource("receiveTask.bpmn")
+            .deploy();
+
+    logger.info("部署id:{}, 部署名称:{}", deployment.getId(), deployment.getName());
+
+    Assert.assertNotNull(deployment);
+  }
+
+  @Test
+  public void processInstance() {
+    String key = "receiveTask";
+    ProcessInstance processInstance = processEngine.getRuntimeService()
+            .startProcessInstanceByKey(key);
+
+    logger.info("流程实例id:{}, 流程实例名称:{}, 流程定义id", processInstance.getId(), processInstance.getName(), processInstance.getProcessDefinitionId());
+
+    // 查执行对象
+    Execution execution = processEngine.getRuntimeService()
+            .createExecutionQuery()
+            .processInstanceId(processInstance.getId())
+            .activityId("_4")
+            .singleResult();
+
+    logger.info("执行对象id:{}", execution.getId());
+
+    // 设置变量
+    processEngine.getRuntimeService()
+            .setVariable(execution.getId(), "当日销售额", 22);
+
+    // 向后执行一步
+    processEngine.getRuntimeService()
+            .signalEventReceived(execution.getId());
+  }
+
+  @Test
+  public void identity() {
+  }
+}
