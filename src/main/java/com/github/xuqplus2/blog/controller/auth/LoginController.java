8afb5c3ecc3da3164c10b98864d13049bcae5313
xjplus <xxjplus@github.com>
2019-08-11 23:56:54 +0800

    oAuth2ClientContext.set(null), sso单点登出...

--- src/main/java/com/github/xuqplus2/blog/controller/auth/LoginController.java
+++ src/main/java/com/github/xuqplus2/blog/controller/auth/LoginController.java
@@ -2,10 +2,14 @@ package com.github.xuqplus2.blog.controller.auth;
 
 import com.github.xuqplus2.blog.vo.resp.BasicResp;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,9 @@ import javax.transaction.Transactional;
 @RequestMapping("auth")
 public class LoginController {
 
+    @Autowired
+    OAuth2ClientContext oAuth2ClientContext;
+
     @RequestMapping(value = "currentUser", method = {RequestMethod.GET, RequestMethod.POST}, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
     public ResponseEntity currentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -26,9 +33,10 @@ public class LoginController {
     @Transactional
     @RequestMapping(value = "logout", method = {RequestMethod.GET, RequestMethod.POST}, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
     public ResponseEntity logout() {
+        // 删除accessToken
+        oAuth2ClientContext.setAccessToken(null);
         // 清除状态
         SecurityContextHolder.clearContext();
-        SecurityContextHolder.getContext().setAuthentication(null);
         return BasicResp.ok();
     }
 }
