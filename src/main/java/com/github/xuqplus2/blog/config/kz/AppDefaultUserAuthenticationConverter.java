8c04175aa028eff01fa97efd4d6d53675c517364
xjplus <xxjplus@github.com>
2019-08-31 22:45:53 +0800

    fix 读不到oauth用户权限的问题

--- src/main/java/com/github/xuqplus2/blog/config/kz/AppDefaultUserAuthenticationConverter.java
+++ src/main/java/com/github/xuqplus2/blog/config/kz/AppDefaultUserAuthenticationConverter.java
@@ -4,9 +4,12 @@ import com.alibaba.fastjson.JSON;
 import com.github.xuqplus2.blog.vo.resp.CurrentUser;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 替换默认的<code>extractAuthentication</code>方法
@@ -19,6 +22,12 @@ public class AppDefaultUserAuthenticationConverter extends DefaultUserAuthentica
         Authentication authentication = super.extractAuthentication(map);
         if (map.containsKey("currentUser")) {
             CurrentUser principal = JSON.parseObject(JSON.toJSONString(map.get("currentUser")), CurrentUser.class);
+            if (null == authentication.getAuthorities() || authentication.getAuthorities().size() == 0) {
+                List<SimpleGrantedAuthority> collect = principal.getAuthorities().stream().map(authority -> {
+                    return new SimpleGrantedAuthority(authority);
+                }).collect(Collectors.toList());
+                return new UsernamePasswordAuthenticationToken(principal, authentication.getCredentials(), collect);
+            }
             return new UsernamePasswordAuthenticationToken(principal, authentication.getCredentials(), authentication.getAuthorities());
         }
         return authentication;
