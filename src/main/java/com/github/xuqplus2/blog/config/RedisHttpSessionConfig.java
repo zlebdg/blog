43026a051c6e4476cb982bc7da9ca9bf2159a4c1
qunqun.xu 许群群 <qunqun.xu@qiyuesuo.com>
2020-02-13 09:44:52 +0800

    spring session with jedis/redis cluster

---
+++ src/main/java/com/github/xuqplus2/blog/config/RedisHttpSessionConfig.java
@@ -0,0 +1,8 @@
+package com.github.xuqplus2.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisHttpSessionConfig {}
